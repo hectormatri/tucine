807303c5a477e1ff473974fd52a53f38632f068e
unknown <hector.martinez.tr@gmail.com>
2023-06-06 16:09:23 +0200

    Añadiendo varible

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
 import { Outlet } from "react-router-dom";
 import NavBar from "./components/NavBar";
 
+
+interface options {
+  method: string,
+  headers: object,
+}
+
+const options:options = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      `Bearer ` + import.meta.env.VITE_APP_API_KEY,
+  },
+};
+
+
 function App() {
+  
+
   return (
     <div className="w-screen h-screen">
       <NavBar />
